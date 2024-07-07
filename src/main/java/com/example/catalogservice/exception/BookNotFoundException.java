9b9d178071d02cf39868022a724aacec094bdb41
git-ly <1786066102@qq.com>
2024-07-07 08:34:13 +0800

    first commit

---
+++ src/main/java/com/example/catalogservice/exception/BookNotFoundException.java
@@ -0,0 +1,7 @@
+package com.example.catalogservice.exception;
+
+public class BookNotFoundException extends RuntimeException{
+    public BookNotFoundException(String isbn){
+        super("The book with ISBN " + isbn + " was not found.");
+    }
+}
---
+++ src/test/java/com/example/catalogservice/web/BookControllerMvcTests.java
@@ -0,0 +1,31 @@
+package com.example.catalogservice.web;
+
+import com.example.catalogservice.domain.BookService;
+import com.example.catalogservice.exception.BookNotFoundException;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import static org.mockito.BDDMockito.given;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(BookController.class)
+class BookControllerMvcTests {
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private BookService bookService;
+
+    @Test
+    void whenGetBookNotExistingThenShouldReturn404() throws Exception {
+        String isbn = "73737313940";
+        given(bookService.viewBookDetails(isbn))
+                .willThrow(BookNotFoundException.class);
+        mockMvc.perform(MockMvcRequestBuilders.get("/books/" + isbn))
+                .andExpect(status().isNotFound());
+    }
+}
